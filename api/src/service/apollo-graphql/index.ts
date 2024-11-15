11d79dd7d9cd09d1a6fe73707de364176ee23ad1
bobbychao-pcln <46798524+bobbylkchao@users.noreply.github.com>
2024-11-15 10:53:44 -0600

    fix: api deployment script

--- api/src/service/apollo-graphql/index.ts
+++ api/src/service/apollo-graphql/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
 import { GraphQLError } from 'graphql'
-import { Express, Request, Response } from 'express'
+import { Express, RequestHandler } from 'express'
 import { typeDefs } from '../../schema'
 import { resolvers } from '../../resolver'
 import { isTrafficAllowed } from '../../misc/is-traffic-allowed'
@@ -16,22 +16,24 @@ export const startApolloServer = async (express: Express) => {
 
   await apolloServer.start()
 
-  express.use('/graphql', expressMiddleware(apolloServer, {
-    context: async ({ req }) => {
-      const origin = (req.headers.origin || req.headers.referer) || ''
-      let authorizationToken = req?.headers?.authorization || ''
-      const { isAllowed, authToken } = isTrafficAllowed(origin, authorizationToken)
-
+  const apolloMiddleware = expressMiddleware(apolloServer, {
+    context: async ({ req }: { req: any }) => {
+      const origin = (req.headers.origin || req.headers.referer) || '';
+      let authorizationToken = req.headers.authorization || '';
+      const { isAllowed, authToken } = isTrafficAllowed(origin, authorizationToken);
+  
       if (!isAllowed || !authToken) {
         throw new GraphQLError('User is not authenticated', {
           extensions: {
             code: 'UNAUTHENTICATED',
             http: { status: 401 },
           },
-        })
+        });
       }
   
-      return { authToken }
+      return { authToken };
     },
-  }))
+  }) as RequestHandler
+
+  express.use('/graphql', apolloMiddleware)
 }
